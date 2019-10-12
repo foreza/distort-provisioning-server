c55143b7cdbb098d7f2dd58f07b2c85542aabdca
Jason Chiu <jason.chiu@inmobi.com>
2019-10-11 21:00:26 -0700

    Add additional GET/POST tests

--- app/tests/smoketest.js
+++ app/tests/smoketest.js
@@ -16,3 +16,29 @@ describe('GET /api/distort/<somebogusvalue>', function() {
     });
 });
 
+// This test shall do a POST to /api/distort/12345 
+describe('POST followed by GET on a session', function() {
+    it('[Expect to post the session]', function (done) {
+        request(app)
+        .post('/api/distort/')
+        .send({"broadcastUID":"12345", "broadcastText":"mochaTestPOST"})
+        .expect(201)
+        // .expect('Content-Type', /json/)
+        .end(function(err, res) {
+            if (err) done(err);
+        });
+
+        done();
+    })
+});
+
+
+// This test should test the base /api route to ensure nothing is being returned
+describe('GET /api/distort', function() {
+    it('[Expect to get the session]', function(done) {
+    request(app).get('/api/distort/123456', function (err, response, body){
+    }).expect(404, done);
+    })
+    
+});
+
